0505cad8054d7711048992ecb2e0e4aab0d1e89e
sri <sri@MacBook-Pro.local>
2026-05-13 13:09:09 +0530

    Updated Azure SQL connection

--- obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39492625adb34804e156cdb3b9e22b7dfa71e34e")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
